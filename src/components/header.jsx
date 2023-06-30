6712809908a5c4553f9ea868d3bcddfed19955e0
canniboal <138153923+canniboal@users.noreply.github.com>
2023-06-30 14:45:05 +0100

    x
     x

---
+++ src/components/header.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import '../App.css';
+import textBoxMedium from "../images/TEXTBOX_MEDIUM.png";
+import revokeFullSpecial from "../images/REVOKE_Button_vers2.png";
+
+class header extends Component {
+
+    revokeAll = () => {
+        let buttons = document.getElementsByName('revoke');
+        for(let button of buttons) {
+            button.click();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div  className="jumbotron">
+                    <div id="titles">
+                        <h2>SUSUWatari - Dust collector</h2>
+                    </div>
+                </div>
+                <div id="columns">
+                    <div id="headers">
+                        <div className="container">
+                            <div className="centered-white">CONTRACT</div>
+                        </div>
+                        <div className="container">
+                            <div className="centered-white">SPENDER</div>
+                        </div>
+                        <div className="container">
+                            <div className="centered-white">ALLOWANCE</div>
+                        </div>
+                        <div className="container">
+                            <div className="centered-white">ACTION</div>
+                        </div>
+                        <div className="container">
+                            <div className="centered-white">MISCELLANEOUS</div>
+                        </div>
+                    </div>
+                </div>
+                <h3 id="loading" hidden>Loading, please wait...</h3>
+                <div id="revokeAll">
+                    <div className="container">
+                        <img className="container" src={textBoxMedium} alt=""/>
+                        <div className="centered">ALL</div>
+                    </div>
+                    <div className="container">
+                        <img className="container" src={textBoxMedium} alt=""/>
+                        <div className="centered">ALL</div>
+                    </div>
+                    <div className="container">
+                        <img className="container" src={textBoxMedium} alt=""/>
+                        <div className="centered">ALL</div>
+                    </div>
+                    <div className="container" onClick={this.revokeAll}>
+                        <img className="container" src={revokeFullSpecial} alt=""/>
+                        <div className="centered-white"><a id="revokeLink" onClick={this.revokeAll}>Revoke All</a></div>
+                    </div>
+                    <div className="container">
+                        <img className="container" src={textBoxMedium} alt=""/>
+                        <div className="centered-white"><a onClick={() => { window.open("mailto:support@susuwatari.com") } }>Report an issue</a></div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default header;
